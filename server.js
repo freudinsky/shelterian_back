a4b238a1e2c8fedd9f7c99f87de1cb7a53e5989a
freudinsky <timo.fuchs94@icloud.com>
2023-11-06 21:17:14 +0100

    cors update

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ import { upload } from "./middlewares/imageHandler.js";
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors({ origin: "http://localhost:5173/", credentials: true }));
+app.use(
+	cors({
+		origin: ["http://localhost:5173/", "https://www.shelterian.com/"],
+		credentials: true,
+	})
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(upload.array("images[]"));
