656fccadf49988ba81c58938d1b77a6456caeaef
freudinsky <timo.fuchs94@icloud.com>
2023-11-06 21:16:06 +0100

    ..

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import { upload } from "./middlewares/imageHandler.js";
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173/", credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(upload.array("images[]"));
