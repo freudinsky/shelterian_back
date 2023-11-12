c89cdcc334f7ba4b8ebbe0007f2e047331aebfd2
freudinsky <me@timo-fuchs.com>
2023-11-12 15:06:52 +0100

    gmail sending

---
+++ utils/gmailAPI.js
@@ -0,0 +1,55 @@
+import { google } from "googleapis";
+
+export const sendEmail = (recipient, subject, emailContent) => {
+	const jwtClient = new google.auth.JWT({
+		email: process.env.SERVICE_ACC_EMAIL,
+		key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+		scopes: [process.env.SCOPE],
+		subject: process.env.USER_MAIL,
+	});
+
+	jwtClient.authorize((err, tokens) => {
+		if (err) {
+			console.error("Error: ", err);
+			return;
+		}
+	});
+
+	const gmail = google.gmail({ version: "v1", auth: jwtClient });
+
+	const headers = {
+		To: recipient,
+		Subject: subject,
+		From: "Shelterian <info@shelterian.com>",
+		"Content-Type": "text/html; charset=utf-8",
+	};
+
+	const email = Object.keys(headers)
+		.map((header) => `${header}: ${headers[header]}`)
+		.join("\r\n");
+
+	const fullEmail = `${email}\r\n\r\n${emailContent}`;
+
+	const base64EncodedEmail = Buffer.from(fullEmail)
+		.toString("base64")
+		.replace(/\+/g, "-")
+		.replace(/\//g, "_");
+
+	gmail.users.messages.send(
+		{
+			userId: "me",
+			requestBody: {
+				raw: base64EncodedEmail,
+				userId: "me",
+			},
+		},
+		(err, res) => {
+			if (err) {
+				console.error("Error: ", err);
+				return;
+			}
+			console.log("Email sent: ", res.data);
+		}
+	);
+};
+
